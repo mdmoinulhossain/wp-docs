0fb9dd7b373345213379762cc0d3dd7c84f123a7
MOHAMMAD MOINUL HOSSAIN <57934796+mdmoinulhossain@users.noreply.github.com>
2024-11-16 18:59:41 +0600

    extra info added

--- development/theme-dev/Child Theme/functions.php
+++ development/theme-dev/Child Theme/functions.php
@@ -11,3 +11,17 @@ function total_child_enqueue_parent_theme_style() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'total_child_enqueue_parent_theme_style' );
+
+
+
+
+// <?php
+
+// /* Function to enqueue stylesheet from parent theme */
+
+// function child_enqueue__parent_scripts() {
+
+//     wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );
+
+// }
+// add_action( 'wp_enqueue_scripts', 'child_enqueue__parent_scripts' );
